bc08ffbf0eb49d10d5e5c057679103916c4b59df
JonasMahlburg <darrylmanga@yahoo.com>
2025-11-11 10:02:21 +0100

    Initial Commit

---
+++ Restaurant_Finder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Restaurant_Finder
+//
+//  Created by Jonas Mahlburg on 11.11.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Restaurant_Finder/Restaurant_FinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restaurant_FinderApp.swift
+//  Restaurant_Finder
+//
+//  Created by Jonas Mahlburg on 11.11.25.
+//
+
+import SwiftUI
+
+@main
+struct Restaurant_FinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
